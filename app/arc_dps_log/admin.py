c3cc87829cc77b743f037ecf68890e254de24fc6
Velentine <quitevi@gmail.com>
2022-09-12 18:09:38 +0300

    added: LocalLog model

--- app/arc_dps_log/admin.py
+++ app/arc_dps_log/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
+from app.arc_dps_log.models import LocalLog
+
+
+@admin.register(LocalLog)
+class LocalLogAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "file_name",
+        "dps_report_name",
+        "dps_report_status",
+        "bfi_status",
+    )
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    def has_add_permission(self, request, obj=None):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
--- app/arc_dps_log/models.py
+++ app/arc_dps_log/models.py
@@ -1,3 +1,44 @@
 from django.db import models
 
-# Create your models here.
+from app.arc_dps_log.logs_constants import LogsConstants
+
+
+class LocalLog(models.Model):
+    file_name = models.CharField(
+        max_length=30,
+        unique=True,
+        db_index=True,
+    )
+    file_path = models.CharField(
+        max_length=1000,
+    )
+    file_time = models.DateTimeField(
+        default=None,
+        null=True,
+    )
+
+    dps_report_status = models.PositiveIntegerField(
+        choices=LogsConstants.UPLOAD_STATUS_CHOICES,
+        default=LogsConstants.UPLOAD_STATUS_PENDING,
+        db_index=True,
+    )
+    dps_report_name = models.CharField(
+        max_length=50,
+    )
+
+    bfi_status = models.PositiveIntegerField(
+        choices=LogsConstants.UPLOAD_STATUS_CHOICES,
+        default=LogsConstants.UPLOAD_STATUS_PENDING,
+        db_index=True,
+    )
+    bfi_fight_id = models.PositiveBigIntegerField(
+        default=None,
+        null=True,
+        blank=True,
+    )
+
+    def __str__(self):
+        return f"LocalLog '{self.file_name}'"
+
+    class Meta:
+        ordering = ("-file_time",)
