caefaa66d8e985aa94f7a535bab27053b2315bf2
Velentine <quitevi@gmail.com>
2022-09-13 16:00:25 +0300

    upd: LogsListView with pagination

--- app/arc_dps_log/views.py
+++ app/arc_dps_log/views.py
@@ -1,24 +1,11 @@
-from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import ListView
 
 from app.arc_dps_log.models import LocalLog
 
 
-class LogsListView(TemplateView):
+class LogsListView(ListView):
     template_name = "logs_list.html"
-
-    def get(self, request, *args, **kwargs):
-        logs_list = []
-        for log_obj in LocalLog.objects.defer("file_time").all()[:20]:
-            logs_list.append({
-                "id": log_obj.id,
-                "file_name": log_obj.file_name,
-                "file_path": log_obj.file_path,
-                "dps_report_status": log_obj.dps_report_status,
-                "dps_report_status_name": log_obj.get_dps_report_status_display(),
-                "dps_report_name": log_obj.dps_report_name,
-                "bfi_status": log_obj.bfi_status,
-                "bfi_status_name": log_obj.get_bfi_status_display(),
-                "bfi_fight_id": log_obj.bfi_fight_id,
-            })
-        return render(request, self.template_name, {'logs_list': logs_list})
+    model = LocalLog
+    paginate_by = 15
+    context_object_name = "logs_list"
+    queryset = LocalLog.objects.defer("file_path", "file_time")
