7bec1e903354f8643b14e8e0899d49e8fb3fb952
Valentin <quitevi@gmail.com>
2022-09-24 15:27:40 +0300

    upd: requirements.txt

--- dj_settings/__init__.py
+++ dj_settings/__init__.py
@@ -1,8 +1,5 @@
-from __future__ import absolute_import, unicode_literals
-
 # This will make sure the app is always imported when
 # Django starts so that shared_task will use this app.
 from celery_scripts.celery_app import celery_app
-from dj_settings.settings import *
 
 __all__ = ("celery_app",)
